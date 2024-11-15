e4401e05e93a77d9c57a64f6fc4c4099421ea6bb
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-11-15 16:33:43 +0100

    Finalize design of app and improve db seed to reach 60 movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,6 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-# Call API to retrieve top movies
-url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
-
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-
-request = Net::HTTP::Get.new(url)
-request["accept"] = 'application/json'
-request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWRiYzcyOGFiOTNiY2QwNThjYmU2MTYzZmVhMjVjYyIsIm5iZiI6MTczMTU4MzgxNi44ODE4OTk2LCJzdWIiOiI2NzM1ZGVhNTI5NTRkMjY0NzYyNTgzMzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0xJCgvU4iu_NJI6ujPKgZVsEp410dcLxKVAmUIFt9Vc'
-
-response = http.request(request).read_body
-movies = JSON.parse(response)["results"]
 
 # Remove previously created movies
 puts "Destroying all previously created movies, bookmarks and lists ..."
@@ -23,25 +11,46 @@ Movie.destroy_all
 puts "#{Movie.count} movies left in DB"
 
 # Itterate on results to create movies
-puts "Create #{movies.count} movies ..."
-movies.each_with_index do |movie, index|
-  title = movie["title"]
-  overview = movie["overview"]
-  poster_url = "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}"
-  rating = movie["vote_average"]
-  # puts title
-  # puts image
-  # puts rating
-  Movie.create!(
-    title: title,
-    overview:overview,
-    poster_url:poster_url,
-    rating:rating
-  )
-  puts "Movie #{} created, #{index + 1}/#{movies.count}"
-end
+# Call API to retrieve top movies
+
+PAGES = ["1", "2", "3"]
+PAGES.each do |page|
+  url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=#{page}")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(url)
+  request["accept"] = 'application/json'
+  request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWRiYzcyOGFiOTNiY2QwNThjYmU2MTYzZmVhMjVjYyIsIm5iZiI6MTczMTU4MzgxNi44ODE4OTk2LCJzdWIiOiI2NzM1ZGVhNTI5NTRkMjY0NzYyNTgzMzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0xJCgvU4iu_NJI6ujPKgZVsEp410dcLxKVAmUIFt9Vc'
+
+  response = http.request(request).read_body
+  movies = JSON.parse(response)["results"]
+
+  puts "Create #{movies.count} movies ... for page #{page}"
+
+  movies.each_with_index do |movie, index|
+    title = movie["title"]
+    overview = movie["overview"]
+    poster_url = "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}"
+    rating = movie["vote_average"]
+    # puts title
+    # puts image
+    # puts rating
+    Movie.create!(
+      title: title,
+      overview:overview,
+      poster_url:poster_url,
+      rating:rating
+    )
+    puts "Movie #{} created, #{index + 1}/#{movies.count}"
+  end
 
 puts "#{Movie.count} movies created..."
+
+end
+
+
 
 # List.create!(name:"Comedies");
 # List.create!(name:"Horror Movies");
